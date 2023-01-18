136df51fe54b1294f7c45a329643784d93ee731a
JidOsc <84289227+JidOsc@users.noreply.github.com>
2023-01-18 14:40:38 +0100

    character and door test

--- Assets/ScriptCharacter.cs
+++ Assets/ScriptCharacter.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ScriptCharacter : MonoBehaviour
 {
+    public int speed = 1;
+    public new Rigidbody2D rigidbody;
 
     int RandomNumber()
     {
@@ -19,6 +21,22 @@ public class ScriptCharacter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.D))
+        {rigidbody.velocity = new Vector2(speed, 0);}
+
+        else if (Input.GetKey(KeyCode.A))
+        {rigidbody.velocity = new Vector2(-speed, 0);}
+
+        else
+        {rigidbody.velocity = new Vector2(0, 0);}
+
     }
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+        //if(collision.transform.name == "door")
+       // {rigidbody.velocity *= -1;}
+    //}
+
+
 }
