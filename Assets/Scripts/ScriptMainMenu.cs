019645601da6d1be53a693783ce4593d0d980ec4
DemonSlayer69696969 <rutgerfischer500@gmail.com>
2023-02-15 11:07:40 +0100

    instruduction meny
    kol:)

--- Assets/Scripts/ScriptMainMenu.cs
+++ Assets/Scripts/ScriptMainMenu.cs
@@ -6,9 +6,22 @@ using UnityEngine.SceneManagement;
 
 public class ScriptMainMenu : MonoBehaviour
 {
+    //
     public void PlayGame ()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    public void InstructionsGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+
+    }
+
+    public void Backgame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
 
     }
 
